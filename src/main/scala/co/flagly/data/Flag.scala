ff3a14eee97d9f16e4ee11a9bfcff86eb21a7d90
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2019-06-19 15:10:40 +0300

    Reduce Flag to have a Boolean value

--- src/main/scala/co/flagly/data/Flag.scala
+++ src/main/scala/co/flagly/data/Flag.scala
@@ -3,21 +3,16 @@ package co.flagly.data
 import java.time.ZonedDateTime
 import java.util.UUID
 
-sealed trait Flag {
-  type Value
-
-  val id: UUID
-  val name: String
-  val description: String
-  val dataType: DataType
-  val value: Value
-  val createdAt: ZonedDateTime
-  val updatedAt: ZonedDateTime
-}
+final case class Flag(id: UUID,
+                      name: String,
+                      description: String,
+                      value: Boolean,
+                      createdAt: ZonedDateTime,
+                      updatedAt: ZonedDateTime)
 
 object Flag {
-  def boolean(name: String, description: String, value: Boolean): BooleanFlag =
-    BooleanFlag(
+  def apply(name: String, description: String, value: Boolean): Flag =
+    Flag(
       id           = UUID.randomUUID,
       name         = name,
       description  = description,
@@ -25,57 +20,4 @@ object Flag {
       createdAt    = ZonedDateTime.now,
       updatedAt    = ZonedDateTime.now
     )
-
-  def number(name: String, description: String, value: BigDecimal): NumberFlag =
-    NumberFlag(
-      id           = UUID.randomUUID,
-      name         = name,
-      description  = description,
-      value        = value,
-      createdAt    = ZonedDateTime.now,
-      updatedAt    = ZonedDateTime.now
-    )
-
-  def text(name: String, description: String, value: String): TextFlag =
-    TextFlag(
-      id           = UUID.randomUUID,
-      name         = name,
-      description  = description,
-      value        = value,
-      createdAt    = ZonedDateTime.now,
-      updatedAt    = ZonedDateTime.now
-    )
-}
-
-final case class BooleanFlag(override val id: UUID,
-                             override val name: String,
-                             override val description: String,
-                             override val value: Boolean,
-                             override val createdAt: ZonedDateTime,
-                             override val updatedAt: ZonedDateTime) extends Flag {
-  override type Value = Boolean
-
-  override val dataType: DataType = DataType.Boolean
-}
-
-final case class NumberFlag(override val id: UUID,
-                            override val name: String,
-                            override val description: String,
-                            override val value: BigDecimal,
-                            override val createdAt: ZonedDateTime,
-                            override val updatedAt: ZonedDateTime) extends Flag {
-  override type Value = BigDecimal
-
-  override val dataType: DataType = DataType.Number
-}
-
-final case class TextFlag(override val id: UUID,
-                          override val name: String,
-                          override val description: String,
-                          override val value: String,
-                          override val createdAt: ZonedDateTime,
-                          override val updatedAt: ZonedDateTime) extends Flag {
-  override type Value = String
-
-  override val dataType: DataType = DataType.Text
 }
