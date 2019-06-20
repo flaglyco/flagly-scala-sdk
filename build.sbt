144698f8307d4e635f513b7627ed9d2f9309e8ca
Mehmet Akif Tütüncü <m.akif.tutuncu@gmail.com>
2019-06-20 18:48:29 +0300

    INCOMPLETE: Implement Http with sttp

--- build.sbt
+++ build.sbt
@@ -5,7 +5,10 @@ scalaVersion in ThisBuild := "2.12.8"
 lazy val `flagly-scala-sdk` = (project in file("."))
   .settings(
     libraryDependencies ++= Seq(
-      "co.flagly"     %% "flagly-core" % "0.1.0-SNAPSHOT",
-      "org.scalatest" %% "scalatest"   % "3.0.5" % Test
+      "co.flagly"             %% "flagly-core"                      % "0.1.0-SNAPSHOT",
+      "com.softwaremill.sttp" %% "core"                             % "1.6.0",
+      "com.softwaremill.sttp" %% "async-http-client-backend-future" % "1.6.0",
+      "com.softwaremill.sttp" %% "play-json"                        % "1.6.0",
+      "org.scalatest"         %% "scalatest"                        % "3.0.5" % Test
     )
   )
--- src/main/scala/co/flagly/Flagly.scala
+++ src/main/scala/co/flagly/Flagly.scala
@@ -3,9 +3,40 @@ package co.flagly
 import java.util.UUID
 
 import co.flagly.core.{Flag, FlaglyError}
+import co.flagly.utils.ZDT
+import play.api.libs.json.{JsError, JsObject, JsSuccess, Reads}
 
 import scala.concurrent.{ExecutionContext, Future}
+import scala.util.Try
+import scala.util.control.NonFatal
 
-class Flagly(config: SDKConfig) {
-  def getFlag(id: UUID)(implicit ec: ExecutionContext): Future[Option[Flag]] = Future.failed(FlaglyError.of("Not implemented yet!"))
+class Flagly(config: SDKConfig, http: Http) {
+  def getFlag(id: UUID)(implicit ec: ExecutionContext): Future[Option[Flag]] =
+    http.get(s"${config.host}/flags/$id").map(_.asOpt[Flag]).recoverWith {
+      case NonFatal(t) =>
+        Future.failed(FlaglyError.of(503, s"Cannot get flag $id!", t))
+    }
+
+  implicit val flagReads: Reads[Flag] =
+    Reads[Flag] {
+      case json: JsObject =>
+        val maybeFlag = for {
+          id          <- (json \ "id").asOpt[UUID]
+          name        <- (json \ "name").asOpt[String]
+          description <- (json \ "description").asOpt[String]
+          value       <- (json \ "value").asOpt[Boolean]
+          createdAt   <- (json \ "createdAt").asOpt[String].flatMap(zdt => Try(ZDT.fromString(zdt)).toOption)
+          updatedAt   <- (json \ "updatedAt").asOpt[String].flatMap(zdt => Try(ZDT.fromString(zdt)).toOption)
+        } yield {
+          Flag.of(id, name, description, value, createdAt, updatedAt)
+        }
+
+        maybeFlag match {
+          case None       => JsError(s"$json is not a valid Flag!")
+          case Some(flag) => JsSuccess(flag)
+        }
+
+      case json =>
+        JsError(s"$json is not a valid Flag!")
+    }
 }
